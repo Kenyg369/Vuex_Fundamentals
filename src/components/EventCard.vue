e071700eb8a9fa93752a1c66a40d07d1dc3ef272
Kenyg369 <kengrand369@gmail.com>
2021-06-03 16:23:32 -0500

    con

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -15,7 +15,6 @@ export default {
       type: Object ,
       required: true
     }
-
   }
 };
 </script>
---
+++ src/views/EventCreate.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>Create an event</h1>
+
+<div class="form-container">
+  <from @submit.prevent="OnSubmit">
+    <label>Select a category: </label>
+    <select v-model="event.category">
+      <option
+        v-for ="option in categories"
+        :value="option"
+        :key="option"
+        :selected="option" === event.category
+      >{{ option }}</option>
+    </select>
+          
+</template>
